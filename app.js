ca98f5012809b2daa834d2bab36cb594dc947732
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-02 14:57:14 +0530

    Wait for DOMContentLoaded Event

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("I'm here!")
+// defer attribute makes the file to be executed after parsing of document.
+// but DOM may still not be loaded completely
+// So checking for DOMContentLoaded event before DOM manipulation
+// It is still before rendering
+// There is 'load' event which triggers after whole page has loaded
+window.addEventListener('DOMContentLoaded', () => {
+    nav = document.querySelector('nav')
+    console.log(nav)
+})
