24fad0d6e5b53e23b801827c5437c35887085b82
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-06 00:44:20 +0530

    Modular data loading

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import Store from './services/Store.js';
-import API from './services/API.js';
+import { loadData } from './services/Menu.js';
 
 // Since it is a module, hooking to window object to make Store global
 window.app = {
@@ -11,7 +11,6 @@ window.app = {
 // So checking for DOMContentLoaded event before DOM manipulation
 // It is still before rendering
 // There is 'load' event which triggers after whole page has loaded
-window.addEventListener('DOMContentLoaded', async () => {
-    const menu = await API.fetchMenu();
-    console.log(menu);
+window.addEventListener('DOMContentLoaded', () => {
+    loadData();
 });
