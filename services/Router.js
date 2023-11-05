968c959f9bc321017c6a67803dc9344332b83f94
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-06 01:40:53 +0530

    Changing DOM based on route

--- services/Router.js
+++ services/Router.js
@@ -3,7 +3,7 @@ export default {
         document.querySelectorAll("a.navlink").forEach(a => {
             a.addEventListener('click', (event) => {
                 event.preventDefault();
-                const url = a.href;
+                const url = a.getAttribute("href");
                 this.go(url);
             });
         });
@@ -15,6 +15,28 @@ export default {
 
         if (saveHistory) {
             history.pushState({ route }, null, route);
+        }
+
+        let pageElement = null;
+        switch (route) {
+            case "/":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Menu";
+                break;
+            case "/order":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Your Order";
+                break;
+            default:
+                break;
+        }
+
+        if (pageElement) {
+            const mainEl = document.querySelector("main");
+            mainEl.children[0]?.remove();
+            mainEl.appendChild(pageElement);
+            window.scrollX = 0;
+            window.scrollY = 0;
         }
     }
 };
